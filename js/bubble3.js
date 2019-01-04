e497946378f2c4a8548da78d083c9bc0f3cc9bcb
oio154 <oio154@gmail.com>
2019-01-04 13:12:54 +0100

    4-2093482098

--- js/bubble3.js
+++ js/bubble3.js
@@ -34,27 +34,56 @@ function Circle(x,y,dx,dy,radius){
   this.dy=dy
   this.radius=radius
   
-  this.draw = function(){
+  // this.draw = function(){
+  //   c.beginPath();
+  //   c.arc(this.x,this.y, this.radius , Math.PI *2, 0 , false);
+  //   // radius = 20*Math.random();
+  //   c.strokeStyle = `rgba(100,100,100,${(this.radius/20)})`;
+  //   c.fillStyle = `rgba(100,100,100,${(this.radius/20)})`;
+  //   c.fill();
+  //   //c.shadowColor = `#ee22cc`;
+  //   //c.shadowBlur='5'
+  //   c.stroke();
+  // }
+
+
+
+  // this.update = function(){
+  //   if(this.x>innerWidth-this.radius  || this.x<this.radius ) this.dx=-this.dx;
+  //   if(this.y>innerHeight-this.radius  || this.y<this.radius ) this.dy=-this.dy;
+  //   this.x+=this.dx;
+  //   this.y+=this.dy;
+  //   this.draw();
+   
+  // }
+ 
+ 
+}
+
+
+Circle.prototype.update = function(){
+    if(this.x>innerWidth-this.radius  || this.x<this.radius ) this.dx=-this.dx;
+    if(this.y>innerHeight-this.radius  || this.y<this.radius ) this.dy=-this.dy;
+    //gravity
+    // if(Math.round(this.dy)==0) this.dy++;
+    // if(this.dy>0) this.dy=this.dy*1.1
+    // if(this.dy<0) this.dy=this.dy/1.1
+    // console.log(this.y)
+    this.x+=this.dx/12;
+    this.y+=this.dy/12;
+    this.draw();
+}
+
+
+Circle.prototype.draw = function(){
     c.beginPath();
     c.arc(this.x,this.y, this.radius , Math.PI *2, 0 , false);
-    // radius = 20*Math.random();
     c.strokeStyle = `rgba(100,100,100,${(this.radius/20)})`;
     c.fillStyle = `rgba(100,100,100,${(this.radius/20)})`;
     c.fill();
-    //c.shadowColor = `#ee22cc`;
-    //c.shadowBlur='5'
     c.stroke();
   }
-  this.update = function(){
-    if(this.x>innerWidth-this.radius  || this.x<this.radius ) this.dx=-this.dx;
-    if(this.y>innerHeight-this.radius  || this.y<this.radius ) this.dy=-this.dy;
-    this.x+=this.dx;
-    this.y+=this.dy;
-    this.draw();
-  }
 
- 
-}
 
 
 
@@ -70,11 +99,11 @@ var radius = 10;
 
 var circles=[];
 
-/*
-for(var z=0;z<3;z++){
+
+for(var z=0;z<1;z++){
  circles.push(new Circle(innerWidth*Math.random()/8+20,innerHeight*Math.random()/8+20,20*(Math.random() - .5),20*(Math.random() - .5),20*Math.random()));
 }
-*/
+
 
 
 function animate(){
